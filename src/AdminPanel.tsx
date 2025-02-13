0c9db76414cdd355b35bc06af491d3cab2d4535d
Namig <45629136+namikarif@users.noreply.github.com>
2025-02-13 14:25:14 +0400

    Update AdminPanel.tsx

--- src/AdminPanel.tsx
+++ src/AdminPanel.tsx
@@ -1,216 +1,156 @@
-import { useEffect, useRef, useState } from "react";
-import { io } from "socket.io-client";
-import { DeviceDto } from "./models/device";
-import { DeviceState } from "./models/socket.dto";
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import * as fs from 'fs';
+import * as path from 'path';
+import { Server, Socket } from 'socket.io';
+import { DeviceService } from './device.service';
+import { DeviceDto } from '../models/device.dto';
+import { DeviceState, VideoBlobData } from '../models/socket.dto';
+import { StreamDto } from '../models/stream.dto';
 
-const apiUrl = "https://nestjs-socket.onrender.com";
-// const apiUrl = "http://localhost:3001";
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class SocketGateway {
+  @WebSocketServer()
+  server: Server;
 
-export default function AdminPanel() {
-  const socket = io(apiUrl);
-  const [devices, setDevices] = useState<DeviceDto[]>([]);
-  const [powerEvent, setPowerEvent] = useState<string | null>(null);
-  const [selectedDevice, setSelectedDevice] = useState<DeviceDto | null>(null);
-  const [startingView, setStartingView] = useState<boolean>(false);
+  private writeCamera: StreamDto = {
+    stream: undefined,
+    filePath: '',
+    deviceId: '',
+    startTime: 0,
+  };
+  private writeDesktop: StreamDto = {
+    stream: undefined,
+    filePath: '',
+    deviceId: '',
+    startTime: 0,
+  };
 
-  const screenVideoRef = useRef<HTMLVideoElement>(null);
-  const cameraVideoRef = useRef<HTMLVideoElement>(null);
-  const [peerConnection, setPeerConnection] = useState(new RTCPeerConnection());
+  constructor(private deviceService: DeviceService) {}
 
-  useEffect(() => {
-    (async () => {
-      const deviceData: DeviceDto[] = await fetch(`${apiUrl}/devices/list`).then(
-        (response) => response.json(),
+  @SubscribeMessage('register-computer')
+  async handleRegister(
+    @MessageBody() device: DeviceDto,
+    @ConnectedSocket() client: Socket,
+  ) {
+    await this.deviceService.setDevice(device);
+    this.server.emit('update-computers', await this.deviceService.getDevices());
+    await client.join(device.deviceId);
+  }
+
+  @SubscribeMessage('start-stream')
+  handleStartStream(@MessageBody() id: string) {
+    this.server.emit(`start-stream-${id}`);
+  }
+
+  @SubscribeMessage('stop-stream')
+  async handleStopStream(@MessageBody() id: string) {
+    this.writeCamera.stream?.end();
+    this.writeDesktop.stream?.end();
+    const endTime = new Date().getTime();
+
+    await this.deviceService.setDeviceVideo({
+      deviceId: this.writeDesktop.deviceId,
+      videoPath: this.writeDesktop.filePath,
+      videoTime: endTime - this.writeDesktop.startTime,
+    });
+
+    await this.deviceService.setDeviceVideo({
+      deviceId: this.writeCamera.deviceId,
+      videoPath: this.writeCamera.filePath,
+      videoTime: endTime - this.writeCamera.startTime,
+    });
+
+    this.writeCamera = {
+      stream: undefined,
+      filePath: '',
+      deviceId: '',
+      startTime: 0,
+    };
+    this.writeDesktop = {
+      stream: undefined,
+      filePath: '',
+      deviceId: '',
+      startTime: 0,
+    };
+    this.server.emit(`stop-stream-${id}`);
+  }
+
+  @SubscribeMessage('stream-blob-data')
+  handleStreamBlobData(@MessageBody() data: VideoBlobData) {
+    if (data.folder === 'desktop') {
+      this.writeDesktop.startTime = data.startTime;
+      this.writeDesktop.deviceId = data.deviceId;
+      this.writeDesktop.filePath = path.join(
+        __dirname,
+        `../src/uploads/${data.folder}`,
+        `${data.deviceId}.webm`,
       );
-
-      setDevices(deviceData);
-
-      socket.on("computers", (data: DeviceDto[]) => {
-        setDevices(data);
-      });
-
-      socket.on("update-computers", (data: DeviceDto[]) => setDevices(data));
-    })();
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
-
-  useEffect(() => {
-    peerConnection.ontrack = (event) => {
-      const [stream] = event.streams;
-      if (event.track.kind === "video") {
-        if (!screenVideoRef.current?.srcObject) {
-          screenVideoRef.current!.srcObject = stream;
-        } else {
-          cameraVideoRef.current!.srcObject = stream;
-        }
+      if (!this.writeDesktop.stream) {
+        this.writeDesktop.stream = fs.createWriteStream(
+          this.writeDesktop.filePath,
+          {
+            flags: 'a',
+          },
+        );
       }
-    };
 
-    socket.on("offer", async (offer) => {
-      await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+      this.writeDesktop.stream.write(Buffer.from(data.blob));
+    } else {
+      this.writeCamera.startTime = data.startTime;
+      this.writeCamera.deviceId = data.deviceId;
+      this.writeCamera.filePath = path.join(
+        __dirname,
+        `../src/uploads/${data.folder}`,
+        `${data.deviceId}.webm`,
+      );
+      if (!this.writeCamera.stream) {
+        this.writeCamera.stream = fs.createWriteStream(
+          this.writeCamera.filePath,
+          { flags: 'a' },
+        );
+      }
 
-      const answer = await peerConnection.createAnswer();
-      await peerConnection.setLocalDescription(answer);
-
-      socket.emit("answer", answer);
-    });
-
-    socket.on("ice-candidate", async (candidate) => {
-      await peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
-    });
-
-    return () => {
-      peerConnection.close();
-    };
-  }, [peerConnection]);
-
-  useEffect(() => {
-    socket.on("change-state", (data: DeviceState) => {
-      setDevices((prev) => {
-        const updatedDevices = [...prev];
-
-        const deviceIndex = updatedDevices.findIndex((dv) => dv.deviceId === data.id);
-
-        if (deviceIndex !== -1) {
-          updatedDevices[deviceIndex] = {
-            ...updatedDevices[deviceIndex],
-            shutdown: data.shutdown,
-            isEnable: data.isEnable,
-            isLock: data.isLock,
-          };
-        }
-
-        if ((data.isLock || !data.shutdown) && selectedDevice?.deviceId === data.id) {
-          setPowerEvent(
-            data.shutdown ? "The computer is shutting down..." : "The computer is locked.",
-          );
-        } else {
-          setPowerEvent(null);
-        }
-
-        console.log({ updatedDevices });
-        return updatedDevices;
-      });
-    });
-
-    return () => {
-      socket.off("change-state");
-    };
-  }, [devices]);
-
-  const startWatching = () => {
-    if (selectedDevice) {
-      setStartingView(true);
-      setPeerConnection(new RTCPeerConnection());
-      socket.emit(`start-stream`, selectedDevice.deviceId);
+      this.writeCamera.stream.write(Buffer.from(data.blob));
     }
-  };
+  }
 
-  const stopWatching = () => {
-    if (selectedDevice) {
-      if (screenVideoRef.current?.srcObject instanceof MediaStream) {
-        screenVideoRef.current.srcObject.getTracks().forEach(track => track.stop());
-        screenVideoRef.current.srcObject = null;
-      }
+  @SubscribeMessage('change-state')
+  async handleChangeState(@MessageBody() data: DeviceState) {
+    await this.deviceService.changeDeviceState(data);
+    this.server.emit(`change-state`, data);
+  }
 
-      if (cameraVideoRef.current?.srcObject instanceof MediaStream) {
-        cameraVideoRef.current.srcObject.getTracks().forEach(track => track.stop());
-        cameraVideoRef.current.srcObject = null;
-      }
-      setPowerEvent(null);
-      setStartingView(false);
-      socket.emit(`stop-stream`, selectedDevice.deviceId);
-    }
-  };
+  @SubscribeMessage('offer')
+  handleOffer(
+    @MessageBody() offer: any,
+    @ConnectedSocket() client: Socket,
+  ): void {
+    client.broadcast.emit('offer', offer);
+  }
 
-  return (
-    <div className="container">
-      <div className="device-list">
-        <h2>List of devices</h2>
-        <ul>
-          {devices.map((device) => (
-            <li
-              key={device.deviceId}
-              onClick={() => !screenVideoRef?.current?.srcObject && setSelectedDevice(device)}
-            >
-              <div className="device-info">
-                <div className="device-info-detail">
-                  <label>ID:</label>
-                  <span>{device.deviceId}</span>
-                </div>
-                <div className="device-info-detail">
-                  <label>Device Name:</label>
-                  <span>{device.name}</span>
-                </div>
-                <div className="device-info-detail">
-                  <label>Status:</label>
-                  <span
-                    title={
-                      !device.shutdown
-                        ? "The computer is ready for monitoring"
-                        : "The computer is shut down"
-                    }
-                    className={`dot ${!device.shutdown ? "success" : "danger"}-bg`}
-                  ></span>
+  @SubscribeMessage('answer')
+  handleAnswer(
+    @MessageBody() answer: any,
+    @ConnectedSocket() client: Socket,
+  ): void {
+    client.broadcast.emit('answer', answer);
+  }
 
-                  <label>Is Enable:</label>
-                  <span
-                    title={
-                      !device.isEnable && !device.shutdown
-                        ? "Being monitored by someone else"
-                        : "The computer is ready for monitoring"
-                    }
-                    className={`dot ${(device.isEnable && !device.shutdown) ? "success" : "danger"}-bg`}
-                  ></span>
-
-                  <label>Is Lock:</label>
-                  <span
-                    title={
-                      device.isLock
-                        ? "The computer is locked"
-                        : "The computer is ready for monitoring"
-                    }
-                    className={`dot ${device.isLock ? "danger" : "success"}-bg`}
-                  ></span>
-                </div>
-              </div>
-              {selectedDevice && selectedDevice.deviceId === device.deviceId && (
-                <div className="controls">
-                  <button onClick={startWatching} disabled={device.shutdown || !device.isEnable || startingView}>
-                    Start Watching
-                  </button>
-                  <button className="stop" disabled={!startingView} onClick={stopWatching}>
-                    Stop Watching
-                  </button>
-                </div>
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <div className="video-player">
-        {powerEvent && <PowerNotification message={powerEvent} />}
-
-        <video ref={screenVideoRef} autoPlay playsInline className={powerEvent ? "blurred" : ""} />
-
-        <video ref={cameraVideoRef} autoPlay playsInline className="camera-feed" />
-      </div>
-    </div>
-  );
+  @SubscribeMessage('ice-candidate')
+  handleIceCandidate(
+    @MessageBody() candidate: any,
+    @ConnectedSocket() client: Socket,
+  ): void {
+    client.broadcast.emit('ice-candidate', candidate);
+  }
 }
-
-const PowerNotification = ({ message }: { message: string }) => {
-  return (
-    <div className="power-notification">
-      <div className="icon">⚠️</div>
-      <div className="content">
-        <strong>System Alert</strong>
-        <p>{message}</p>
-      </div>
-    </div>
-  );
-};
